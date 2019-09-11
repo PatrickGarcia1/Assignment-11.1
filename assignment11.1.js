27c3d270ab5547017da71da58126838846fb82f7
PatrickGarcia1 <53844521+PatrickGarcia1@users.noreply.github.com>
2019-09-10 20:05:25 -0400

    Add files via upload

---
+++ assignment11.1.js
@@ -0,0 +1,11 @@
+var tag = document.getElementById("first");
+
+var tag = document.getElementsByClassName("special");
+
+var tag = document.getElementsByTagName("p")
+
+var tag = document.querySelector(".special");
+
+var tag = document.querySelectorAll("#first");
+
+console.log(tag);
